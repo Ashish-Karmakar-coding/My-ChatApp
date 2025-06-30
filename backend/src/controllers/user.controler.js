705bca0030c26c9a061549d29270d9e57593d3d5
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-06-30 13:35:57 +0530

    Add signup logic

--- backend/src/controllers/user.controler.js
+++ backend/src/controllers/user.controler.js
@@ -1,7 +1,39 @@
+import {User} from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 const signup = (req,res) => {
     // Logic for user signup
-    res.send("!    You are in the signup page   !");
+
+    const {username,password , email} = req.body;
+
+    try {
+        if(!username || ! password || ! email) throw new Error("Please provide all the required fields");
+        
+        const isAreadyExist = User.findone({email})
+        if(isAreadyExist) throw new Error("User already exists")
+
+        hashedPassword = bcrypt.hashSync(password, 10);
+
+        const user = new User({
+            username,
+            email,
+            password : hashedPassword
+        })
+
+        user.save()
+            .res.status(201)
+            .json({
+                message: "User created successfully",
+                user: {
+                    id: user._id,
+                    username: user.username,
+                    email: user.email,
+                }
+            })
+    } catch (error) {
+        throw new Error("Error in signup : ", error.message);
+    }
+    
 }
 const login = (req,res) => {
     // Logic for user signup
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import userRouter from './routers/user.router.js';
 
+dotenv.config()
+
 const app = express()
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the Chat App Backend")
 })
 
+app.use(express.json()) // Middleware to parse JSON bodies
 app.use("/api/users", userRouter)
 
 app.listen(PORT,()=>{
