96a229b9d5863839d1a716df0690cb3cb805e183
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-03 19:15:15 +0530

    basic file structure and tailwind config

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  console.log("App rendered");
+  return (
+    <>
+      <div className="bg-amber-500 h-screen">
+      </div>
+    </>
+  );
+}
+
+export default App;
