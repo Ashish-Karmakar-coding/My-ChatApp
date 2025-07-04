c3ac4e091e6c5e82e70a39d48ec427d2ddea834b
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-04 20:40:01 +0530

    setup the authStore.js

--- frontend/src/lib/authStore.js
+++ frontend/src/lib/authStore.js
@@ -1,2 +1,10 @@
 import {create} from 'zustand'
 
+export const useAuthStore = create((set)=>({
+    authUser : null,
+    isLoggingIn : false,
+    isSigningUp : false,
+    
+    isCheckingAuth : true
+
+}))
