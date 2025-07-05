9ad7717f642bcd4a0a765abdda63282cfa93b213
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-05 13:57:43 +0530

    set login and signup function in authStore

--- frontend/src/lib/authStore.js
+++ frontend/src/lib/authStore.js
@@ -1,4 +1,6 @@
 import {create} from 'zustand'
+import {axiosInstance} from '../utils/axiosInstance.js'
+import {toast} from 'react-hot-toast'
 
 export const useAuthStore = create((set)=>({
     authUser : null,
@@ -7,5 +9,34 @@ export const useAuthStore = create((set)=>({
     isUpdatingProfile : false,
     
     isCheckingAuth : true,
+
+    signUp: async (data) => {
+        set({isSigningUp: true});
+        // Simulate API call
+        try {
+            const response = await axiosInstance.post('/auth/signup', data);
+            set({authUser: response.data, isSigningUp: false});
+            toast.success("Sign Up Successful!");
+            
+        } catch (error) {
+            toast.error("Sign Up Failed. Please try again.");
+            console.error("Sign Up Error:", error);
+            set({isSigningUp: false});
+        }
+    },
+    Login: async (data) => {
+        set({isLoggingIn: true});
+        // Simulate API call
+        try {
+            const response = await axiosInstance.post('/auth/login', data);
+            set({authUser: response.data, isLoggingIn: false});
+            toast.success("Log In Successful!");
+
+        } catch (error) {
+            toast.error("Log in Failed. Please try again.");
+            console.error("Log in Error:", error);
+            set({isLoggingIn: false});
+        }
+    },
 
 }))
