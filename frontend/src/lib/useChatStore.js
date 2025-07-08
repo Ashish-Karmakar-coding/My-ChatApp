07a399e609f2a0aca928c92dcad4cc7e24958246
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-08 23:46:53 +0530

    add some obejcts

--- frontend/src/lib/useChatStore.js
+++ frontend/src/lib/useChatStore.js
@@ -3,5 +3,15 @@ import {axiosInstance} from '../axios/axios.js';
 import {toast} from 'react-hot-toast';
 
 export const useChatStore = create((set) => ({
+    messages :[],
+    users : [],
+
+    selectedUser: null,
+
+    isLoadingMessages: false,
+    isLoadingUsers: false,
+
     
+
+
 }))
