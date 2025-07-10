a874918cdfdecb8c26c417fb77a7b394d1c1a91b
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-10 19:21:17 +0530

    add the chat header

--- frontend/src/pages/Home.page.jsx
+++ frontend/src/pages/Home.page.jsx
@@ -1,11 +1,14 @@
-import HomeSkele from "../skeletons/homeSkele";
-import SideBar from "../components/SideBar";
+import HomeSkele from "../skeletons/homeSkele.jsx";
+import SideBar from "../components/SideBar.jsx";
+import Header from "../components/header.jsx";
 
 export default function HomePage() {
   return (
     <div className="flex bg-gray-900 min-h-[92dvh]">
       <SideBar />
-      <HomeSkele />
+      {/* <HomeSkele /> */}
+        <Header />
+       
     </div>
   );
 }
