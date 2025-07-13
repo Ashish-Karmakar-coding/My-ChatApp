2a44ecd47c9c56dac9cbbf0f8789982b5a6124cb
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-13 19:04:59 +0530

    add some properties

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   };
 
   return (
-    <header className="w-full bg-gray-800 border-b border-gray-700 px-6 py-4 flex items-center gap-4 shadow relative top-0 ">
+    <header className="w-full bg-gray-800 border-b border-gray-700 px-6 py-4 flex gap-4 shadow relative top-0 ">
       <img
         src={user.avatar}
         alt={user.fullName}
--- frontend/src/pages/Home.page.jsx
+++ frontend/src/pages/Home.page.jsx
@@ -1,13 +1,18 @@
-// import HomeSkele from "../skeletons/homeSkele.jsx";
+import HomeSkele from "../skeletons/homeSkele.jsx";
 import SideBar from "../components/SideBar.jsx";
 import Header from "../components/header.jsx";
+import useChatstore from "../lib/useChatStore.js";
+import messageComponent from "../components/message.jsx";
 
 export default function HomePage() {
+
+  const {selectedUser, getUsers, users} = useChatstore();
+
+  
+
   return (
     <div className="flex bg-gray-900 min-h-[92dvh]">
       <SideBar />
-      {/* <HomeSkele /> */}
-        <Header />
        
     </div>
   );
