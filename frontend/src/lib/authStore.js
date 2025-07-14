b66d046db24c90ad083a4bf6132720af332c0a6b
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-14 21:55:23 +0530

    add updateProfile in authStore

--- frontend/src/lib/authStore.js
+++ frontend/src/lib/authStore.js
@@ -67,6 +67,18 @@ export const useAuthStore = create((set)=>({
         }finally{
             set({isCheckingAuth: false});
         }
+    },
+    updateProfile: async (data) => {
+        set({isUpdatingProfile: true});
+        try {
+            const response = await axiosInstance.put('/users/update-profile', data);
+            toast.success("Profile Updated Successfully!");
+        } catch (error) {
+            toast.error(error.response.data.message || "Profile Update Failed. Please try again.");
+            console.error("Update Profile Error:", error);
+        } finally {
+            set({isUpdatingProfile: false});
+        }
     }
 
 }))
