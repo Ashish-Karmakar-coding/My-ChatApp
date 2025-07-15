4f9da7c3f2fef23966520971a23933f4cc9f9806
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-07-15 19:33:41 +0530

    change in chat contaianer

--- frontend/src/components/chatContainer.jsx
+++ frontend/src/components/chatContainer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import {useEffect} from 'react'
 import inputComp from './input.jsx'
 import Header from './header.jsx'
 import MessageSkele from '../skeletons/messageSkele.jsx'
 import Message from './Message.jsx'
 
+import useChatStore from '../lib/useChatStore.js'
+
 const chatContailner = () => {
+
+  const {selectedUser,messages,isLoadingMessages,getMessages} = useChatStore();
+
+
+
   return (
     <>
     
